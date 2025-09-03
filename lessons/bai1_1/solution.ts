e16ab8eca1c11671de560895729541c809cee115
TerryNguyen1403 <giangnht1403@gmail.com>
2025-09-03 11:58:54 +0700

    Solution for bai1_1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 
 export type Block = {
   index: number;
@@ -9,5 +10,14 @@ export type Block = {
 
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  // Lấy ra các thuộc tính hiện tại của block
+  const { index, timestamp, transactions, previous_hash } = block;
+  const value = index + timestamp + JSON.stringify(transactions) + previous_hash;
+
+  // Lấy ra chuỗi băm của block
+  const tmp_hash = crypto.createHash('sha256').update(value).digest('hex');
+  
+
+  // So sánh với current_hash
+  return tmp_hash === block.current_hash;
 }
